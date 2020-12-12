1960713784e1cda3a41177c0cd7d588414192fd5
Ranny Clover <36104212+dlOuOlb@users.noreply.github.com>
2020-12-12 12:23:44 +0900

    Add files via upload

---
+++ Let.cs
@@ -0,0 +1,88 @@
+﻿#nullable enable
+
+namespace dlOuOlb
+{
+	using S = System;
+	using Any = System.Collections;
+	using Typed = System.Collections.Generic;
+	using Compiler = System.Runtime.CompilerServices;
+
+	///<summary>This class provides <see langword="static"/> methods for creating a <see cref="Let{T}"/> instance.</summary>
+	public static class Let:S.Object
+	{
+		///<summary>This <see langword="static"/> method creates a singleton, which holds a readonly value.</summary>
+		///<typeparam name="T">The type of the singleton's only component.</typeparam>
+		///<param name="V">The value of the singleton's only component.</param>
+		///<returns>A singleton, which holds a readonly value.</returns>
+		public static Let<T> New<T>(T V) => new Let<T>(V: V);
+		///<summary>This <see langword="static"/> method creates a singleton, which holds a readonly value.</summary>
+		///<typeparam name="T">The type of the singleton's only component.</typeparam>
+		///<param name="V">The singleton to be copied.</param>
+		///<returns>A singleton, which holds a readonly value.</returns>
+		public static Let<T> New<T>(in Let<T> V) => V;
+	}
+
+	///<summary>This struct represents a singleton, which holds a readonly value.</summary>
+	///<typeparam name="T">The type of the singleton's only component.</typeparam>
+	[S.Diagnostics.DebuggerDisplay(value: @"( {"+nameof(V)+@"} )")]
+	public readonly struct Let<T>:S.IDisposable, Typed.IReadOnlyList<T>, Typed.IReadOnlySet<T>, Compiler.ITuple
+	{
+		#region Constructor
+		///<summary>The value of the current singleton's only component.</summary>
+		public readonly T V;
+		///<summary>This constructor creates a new singleton, which holds a readonly value.</summary>
+		///<param name="V">The value of the singleton's only component.</param>
+		public Let(T V) => this.V=V;
+		#endregion
+
+		#region Operator
+		///<summary>This operator takes a value from a singleton.</summary>
+		///<param name="C">The singleton to be unwrapped.</param>
+		public static explicit operator T(in Let<T> C) => C.V;
+		///<summary>This operator wraps a value into a singleton.</summary>
+		///<param name="V">The value to be wrapped.</param>
+		public static explicit operator Let<T>(T V) => new Let<T>(V: V);
+		#endregion
+
+		#region Object
+		///<summary>This method returns a string that represents the current singleton's only component.</summary>
+		///<returns>A string that represents the current singleton's only component.</returns>
+		///<exception cref="S.NullReferenceException"/>
+		public override S.String? ToString() => this.V is S.Object X ? X.ToString() is S.String Y ? $"({Y})" : null : throw new S.NullReferenceException();
+		#endregion
+
+		#region IDisposable
+		void S.IDisposable.Dispose() { return; }
+		#endregion
+
+		#region IEnumerable
+		Any.IEnumerator Any.IEnumerable.GetEnumerator() { yield return this.V; }
+		Typed.IEnumerator<T> Typed.IEnumerable<T>.GetEnumerator() { yield return this.V; }
+		#endregion
+
+		#region IReadOnlyCollection
+		S.Int32 Typed.IReadOnlyCollection<T>.Count => 1;
+		#endregion
+
+		#region IReadOnlyList
+		T Typed.IReadOnlyList<T>.this[S.Int32 I] => 0==I ? this.V : throw new S.IndexOutOfRangeException();
+		#endregion
+
+		#region IReadOnlySet
+		S.Boolean Typed.IReadOnlySet<T>.Contains(T V) => this.V is S.Object X ? X.Equals(obj: V) : throw new S.NullReferenceException();
+		S.Boolean Typed.IReadOnlySet<T>.IsSubsetOf(Typed.IEnumerable<T> P) { if(this.V is S.Object X) { foreach(var V in P) if(X.Equals(obj: V)) return true; else continue; return false; } else throw new S.NullReferenceException(); }
+		S.Boolean Typed.IReadOnlySet<T>.IsSupersetOf(Typed.IEnumerable<T> P) { if(this.V is S.Object X) { foreach(var V in P) if(X.Equals(obj: V)) continue; else return false; return true; } else throw new S.NullReferenceException(); }
+		S.Boolean Typed.IReadOnlySet<T>.IsProperSubsetOf(Typed.IEnumerable<T> P) { if(this.V is S.Object X) { S.Span<S.Boolean> M = stackalloc S.Boolean[2] { false,false }; foreach(var V in P) { var I = X.Equals(obj: V) ? 1 : 0; if(M[index: 1^I]) return true; else M[index: I]=true; } return false; } else throw new S.NullReferenceException(); }
+		S.Boolean Typed.IReadOnlySet<T>.IsProperSupersetOf(Typed.IEnumerable<T> P) { if(this.V is S.Object) { foreach(var _ in P) return false; return true; } else throw new S.NullReferenceException(); }
+		S.Boolean Typed.IReadOnlySet<T>.Overlaps(Typed.IEnumerable<T> P) => (this as Typed.IReadOnlySet<T>).IsSubsetOf(other: P);
+		S.Boolean Typed.IReadOnlySet<T>.SetEquals(Typed.IEnumerable<T> P) => (this as Typed.IReadOnlySet<T>).IsSupersetOf(other: P);
+		#endregion
+
+		#region ITuple
+		S.Int32 Compiler.ITuple.Length => 1;
+		S.Object? Compiler.ITuple.this[S.Int32 I] => 0==I ? this.V : throw new S.IndexOutOfRangeException();
+		#endregion
+	}
+}
+
+#nullable restore
--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # Let
+
 This library imitates readonly local variables abusing some language features.
+
+## Demonstration
+
+Source code:
+
+```cs
+namespace Demo
+{
+	using S = System;
+	using OuO = dlOuOlb;
+	public static class Program
+	{
+		private static readonly S.Random RNG = new S.Random();
+		public static void Main()
+		{
+			// readonly var Bar = RNG.Next(); // No such thing in .NET 5.0 / C# 9.0 !
+
+			// Creating a singleton with a random integer,
+			// which cannot be determined in compile time:
+			// using var Foo = new OuO.Let<S.Int32>(V: RNG.Next()); // Too verbose.
+			// using var Foo = (OuO.Let<S.Int32>)RNG.Next(); // Less verbose.
+			using var Foo = OuO.Let.New(V: RNG.Next());
+
+			// Foo.V=7; // Error!
+			// Foo=OuO.Let.New(V: 7); // Error!
+
+			// Printing the value:
+			S.Console.WriteLine(value: $"Foo = {Foo}");
+			S.Console.WriteLine(value: $"Foo.V = {Foo.V}");
+
+			// If you want to rip off the container:
+			foreach(var Bar in Foo)
+			{
+				// Bar=7; // Error!
+				S.Console.WriteLine(value: $"Bar = {Bar}");
+			}
+
+			return;
+			// Foo.Dispose(), implemented as no-op, would be called behind the scene.
+		}
+	}
+}
+```
+
+Console output: (Just assume that the value was 31 for example.)
+
+```txt
+Foo = (31)
+Foo.V = 31
+Bar = 31
+```
+
+## Gotcha!
+
+A `Let<T>` instance holds an object of type `T` as a `readonly` field `V`, which seems pretty easy until it isn't. Please see this [documentation](https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/readonly "readonly (C# Reference)"):
+
+> A `readonly` field can't be assigned after the constructor exits. This rule has __different__ implications for value types and reference types:
+> - Because value types directly contain their data, a field that is a `readonly` value type is immutable.
+> - Because reference types contain a reference to their data, a field that is a `readonly` reference type must always refer to the same object. That object isn't immutable. The `readonly` modifier prevents the field from being replaced by a different instance of the reference type. __However__, the modifier doesn't prevent the instance data of the field from being modified through the `readonly` field.
+
+## Release Notes
+
+Version 0.1.0.0
+
+- Date: 2020-12-12.
+- The initial release.
